5e88b14d5ac0cbe270643ae74955326f1d83b215
MINSOO CHOI <4987kk@naver.com>
2025-05-07 19:07:06 +0900

    초기 세팅

---
+++ src/main/java/com/project/catxi/CatxiApplication.java
@@ -0,0 +1,13 @@
+package com.project.catxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/catxi/CatxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.catxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
