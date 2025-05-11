46c190cf26dd68126d171cd5893c52962ea0f714
Dongjun <ldj990517@gmail.com>
2025-05-11 15:57:51 +0900

    [REFACT] 에러코드 수정

--- src/main/java/com/project/catxi/common/api/error/MemberErrorCode.java
+++ src/main/java/com/project/catxi/common/api/error/MemberErrorCode.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MemberErrorCode implements ErrorCode{
-	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO404", "멤버를 찾을 수 없습니다."),
-	INVALID_MEMBER_PARAMETER(HttpStatus.BAD_REQUEST, "PORTFOLIO400", "유효하지 않는 멤버입니다");
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "멤버를 찾을 수 없습니다."),
+	INVALID_MEMBER_PARAMETER(HttpStatus.BAD_REQUEST, "MEMBER400", "유효하지 않는 멤버입니다");
 
 	private final HttpStatus httpStatus;
 	private final String code;
