6d13461ddcd28e945695b34de01123c2bb84de95
Dongjun <ldj990517@gmail.com>
2025-05-11 15:59:51 +0900

    [FEAT] 채팅방 생성 api 구현

--- src/main/java/com/project/catxi/chat/controller/ChatController.java
+++ src/main/java/com/project/catxi/chat/controller/ChatController.java
@@ -1,22 +1,38 @@
 package com.project.catxi.chat.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.catxi.chat.dto.RoomCreateReq;
+import com.project.catxi.chat.dto.RoomCreateRes;
 import com.project.catxi.chat.service.ChatMessageService;
+import com.project.catxi.chat.service.ChatRoomService;
 import com.project.catxi.common.api.ApiResponse;
 import com.project.catxi.common.api.CommonPageResponse;
+import com.project.catxi.member.domain.Member;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
 
 	private final ChatMessageService chatMessageService;
+	private final ChatRoomService chatRoomService;
 
-	public ChatController(ChatMessageService chatMessageService) {
+	public ChatController(ChatMessageService chatMessageService, ChatRoomService chatRoomService) {
 		this.chatMessageService = chatMessageService;
+		this.chatRoomService = chatRoomService;
 	}
+
+	@PostMapping("/room/create")
+	public ResponseEntity<ApiResponse<RoomCreateRes>> createRoom(@RequestBody RoomCreateReq roomCreateReq, Member member){
+		RoomCreateRes res = chatRoomService.creatRoom(roomCreateReq, member);
+		return ResponseEntity.ok(ApiResponse.success(res));
+	}
+
 
 }
