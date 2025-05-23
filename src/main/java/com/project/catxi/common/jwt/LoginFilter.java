817cf0448025a6d9e755f09a8ba977725a7fe119
Dobby19 <southvi1@naver.com>
2025-05-24 00:00:28 +0900

    [FEAT] LoginFilter 세팅

--- src/main/java/com/project/catxi/common/jwt/LoginFilter.java
+++ src/main/java/com/project/catxi/common/jwt/LoginFilter.java
@@ -1,5 +1,45 @@
 package com.project.catxi.common.jwt;
 
-public class LoginFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final AuthenticationManager authenticationManager;
+
+  public LoginFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+  }
+
+  @Override
+  public Authentication attemptAuthentication(
+      HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+    //Client Req -> username, userPw distract
+    String username = obtainUsername(request);
+    String password = obtainPassword(request);
+
+    // System.out.println(username);
+
+    //꺼낸 값 기반 인증 진행
+    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+    return authenticationManager.authenticate(authToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+    System.out.println("success");
+  }
+
+  @Override
+  protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+    System.out.println("fail");
+  }
 }
