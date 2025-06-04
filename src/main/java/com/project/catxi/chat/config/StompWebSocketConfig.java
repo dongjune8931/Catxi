c7f95d983a7d205947efa1b796fe28370d06a25a
DongJun Lee <164463609+dongjune8931@users.noreply.github.com>
2025-06-05 04:10:50 +0900

    Update StompWebSocketConfig.java

--- src/main/java/com/project/catxi/chat/config/StompWebSocketConfig.java
+++ src/main/java/com/project/catxi/chat/config/StompWebSocketConfig.java
@@ -20,6 +20,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/connect")
+			.setAllowedOrigins("http://localhost:5173", "https://catxi.kro.kr")
 			.withSockJS();
 	}
 
