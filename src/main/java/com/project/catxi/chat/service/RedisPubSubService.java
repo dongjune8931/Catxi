39566f176aa04df80e1367b7dd3980fd0e33c6d9
Dongjun <ldj990517@gmail.com>
2025-06-05 01:20:47 +0900

    [REFACT] 레디스 펍섭을 위한 설정 추가 및 stomp 컨트롤러 코드 수정

---
+++ src/main/java/com/project/catxi/chat/service/RedisPubSubService.java
@@ -0,0 +1,40 @@
+package com.project.catxi.chat.service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.catxi.chat.dto.ChatMessageSendReq;
+
+@Service
+public class RedisPubSubService implements MessageListener {
+	private final SimpMessageSendingOperations messageTemplate;
+	public final StringRedisTemplate stringRedisTemplate;
+
+	public RedisPubSubService(@Qualifier("chatPubSub")StringRedisTemplate stringRedisTemplate,SimpMessageSendingOperations messageTemplate) {
+		this.messageTemplate = messageTemplate;
+		this.stringRedisTemplate = stringRedisTemplate;
+	}
+
+	public void publish(String channel,String message){
+		stringRedisTemplate.convertAndSend(channel,message);
+	}
+
+	@Override
+	//pattern 에는 topic의 이름의 패턴이 담겨있고 이 패턴을 기반으로 다이나믹한 코딩
+	public void onMessage(Message message, byte[] pattern) {
+		String payload = new String(message.getBody());
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			ChatMessageSendReq chatMessageDto  = objectMapper.readValue(payload, ChatMessageSendReq.class);
+			messageTemplate.convertAndSend("/topic/"+chatMessageDto.roomId(), chatMessageDto);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/main/java/com/project/catxi/common/config/RedisConfig.java
@@ -0,0 +1,65 @@
+package com.project.catxi.common.config;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+import com.project.catxi.chat.service.RedisPubSubService;
+
+@Configuration
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	//연결 기본 객체
+	@Bean
+	@Qualifier("chatRedisConnectionFactory")
+	public RedisConnectionFactory chatPubSubFactory() {
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+		configuration.setHostName(host);
+		configuration.setPort(port);
+		//redis pub/sub 에서는 특정 데이터베이스에 의존적이지 않음.
+		//configuration.setDatabase(0);
+		return new LettuceConnectionFactory(configuration);
+	}
+
+	//publish 객체
+	@Bean
+	@Qualifier("chatPubSub")
+	// 일반적으로 RedisTemplate< key 데이터 타입 , value 데이터타입> 을 사용
+	public StringRedisTemplate stringRedisTemplate( @Qualifier("chatRedisConnectionFactory")RedisConnectionFactory redisConnectionFactory) {
+		return new StringRedisTemplate(redisConnectionFactory);
+	}
+
+	//subscribe 객체
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(
+		@Qualifier("chatRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory,
+		MessageListenerAdapter messageListenerAdapter
+	) {
+		RedisMessageListenerContainer container=new RedisMessageListenerContainer();
+		container.setConnectionFactory(redisConnectionFactory);
+		container.addMessageListener(messageListenerAdapter, new PatternTopic("chat"));
+		return container;
+	}
+
+
+	//redis에서 수신된 메시지를 처리하는 객체 생성
+	@Bean
+	public MessageListenerAdapter messageListenerAdapter(RedisPubSubService redisPubSubService){
+		//RedisPubSubService의 특정 메서드가 수신된 메시지를 처리할 수 있도록 지정
+		return new MessageListenerAdapter(redisPubSubService,"onMessage");
+	}
+}
