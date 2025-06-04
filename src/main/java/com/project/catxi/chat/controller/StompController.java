ae88681dc53f9ae5ddc59acb50e7b62b5e7a1e3f
Dongjun <ldj990517@gmail.com>
2025-06-05 01:20:47 +0900

    [REFACT] 레디스 펍섭을 위한 설정 추가 및 stomp 컨트롤러 코드 수정

--- src/main/java/com/project/catxi/chat/controller/StompController.java
+++ src/main/java/com/project/catxi/chat/controller/StompController.java
@@ -8,21 +8,33 @@ import org.springframework.stereotype.Controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.catxi.chat.dto.ChatMessageReqDto;
+import com.project.catxi.chat.dto.ChatMessageSendReq;
+import com.project.catxi.chat.service.ChatMessageService;
+import com.project.catxi.chat.service.RedisPubSubService;
 
 @Controller
 public class StompController {
 
 	private final SimpMessageSendingOperations messageTemplate;
+	private final ChatMessageService chatMessageService;
+	private final RedisPubSubService pubSubService;
 
-	public StompController(SimpMessageSendingOperations messageTemplate) {
+	public StompController(SimpMessageSendingOperations messageTemplate, ChatMessageService chatMessageService,
+		RedisPubSubService pubSubService) {
 		this.messageTemplate = messageTemplate;
+		this.chatMessageService = chatMessageService;
+		this.pubSubService = pubSubService;
 	}
 
+
+
 	@MessageMapping("/{roomId}")
-	public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) {
-		System.out.println(chatMessageReqDto.getMessage());
-		//해당 roomId에 메시지를 방행하여 구독중인 클라이언트에게 메시지 전송
-		messageTemplate.convertAndSend("/topic/" + roomId, chatMessageReqDto);
+	public void sendMessage(@DestinationVariable Long roomId, ChatMessageSendReq chatMessageSendReq) throws JsonProcessingException {
+		System.out.println(chatMessageSendReq.message());
+		chatMessageService.saveMessage(roomId, chatMessageSendReq);
+		//messageTemplate.convertAndSend("/topic/"+ roomId,chatMessageSendReq);
+		ObjectMapper objectMapper = new ObjectMapper();
+		String message = objectMapper.writeValueAsString(chatMessageSendReq);
+		pubSubService.publish("chat", message);
 	}
 }
