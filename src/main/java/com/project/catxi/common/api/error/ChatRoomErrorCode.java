1490471e3b15850152bc419bf949c1b7376e0320
LGY010011 <gylm1111pm@gmail.com>
2025-06-09 21:28:15 +0900

    [FEAT] ChatRoom, ChatParticipant ErrorCode 추가

--- src/main/java/com/project/catxi/common/api/error/ChatRoomErrorCode.java
+++ src/main/java/com/project/catxi/common/api/error/ChatRoomErrorCode.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ChatRoomErrorCode implements ErrorCode{
 
+	CHATROOM_NOT_WAITING(HttpStatus.BAD_REQUEST, "CHATROOM400", "채팅방이 대기 상태가 아닙니다."),
 	CHATROOM_FULL(HttpStatus.NOT_FOUND, "CHATROOM405", "채팅방에 인원이 꽉찼습니다."),
 	CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "CHATROOM404", "채팅방를 찾을 수 없습니다."),
 	NOT_OWNED_CHATROOM(HttpStatus.FORBIDDEN, "CHATROOM403", "본인 소유의 채팅방이 아닙니다."),
