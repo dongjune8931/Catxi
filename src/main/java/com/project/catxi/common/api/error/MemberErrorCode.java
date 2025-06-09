19016d94140a3a0ec44fb2c82520dc6b7e2965b5
Dobby19 <southvi1@naver.com>
2025-06-09 10:47:21 +0900

    [FIX] Redirect URI 수정

--- src/main/java/com/project/catxi/common/api/error/MemberErrorCode.java
+++ src/main/java/com/project/catxi/common/api/error/MemberErrorCode.java
@@ -12,8 +12,9 @@ public enum MemberErrorCode implements ErrorCode{
 	INVALID_MEMBER_PARAMETER(HttpStatus.BAD_REQUEST, "MEMBER400", "유효하지 않는 멤버입니다"),
 	DUPLICATE_MEMBER_STUDENTNO(HttpStatus.BAD_REQUEST, "MEMBER401", "이미 가입된 학번입니다"),
 
-	ACCESS_EXPIRED(HttpStatus.BAD_REQUEST,"ACCESS401","액세스 토큰이 만료되었습니다");
-
+	ACCESS_EXPIRED(HttpStatus.BAD_REQUEST,"ACCESS401","액세스 토큰이 만료되었습니다"),
+	DUPLICATE_AUTHORIZE_CODE(HttpStatus.BAD_REQUEST,"ACCESS402","인가 코드 중복 사용");
+	
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
