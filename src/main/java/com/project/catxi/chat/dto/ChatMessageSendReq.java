12c818a1d763552a6c78ff199d365aeea098c98c
DongJun Lee <164463609+dongjune8931@users.noreply.github.com>
2025-06-13 04:05:32 +0900

    Update ChatMessageSendReq.java

--- src/main/java/com/project/catxi/chat/dto/ChatMessageSendReq.java
+++ src/main/java/com/project/catxi/chat/dto/ChatMessageSendReq.java
@@ -1,14 +1,11 @@
 package com.project.catxi.chat.dto;
 
-import com.project.catxi.common.domain.MessageType;
-
 public record ChatMessageSendReq(
 	Long roomId,
 	String email,
-	String message,
-	MessageType msgType
+	String message
 ) {
 	public ChatMessageSendReq withRoomId(Long newRoomId) {
-		return new ChatMessageSendReq(newRoomId, this.email, this.message,this.msgType);
+		return new ChatMessageSendReq(newRoomId, this.email, this.message);
 	}
 }
