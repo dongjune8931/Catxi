898224b6a865a8961c2cd334ada7411ade8dba1c
Dongjun <ldj990517@gmail.com>
2025-06-13 03:36:13 +0900

    강퇴,입장,나가기 시스템메시지 추가

--- src/main/java/com/project/catxi/chat/dto/ChatMessageSendReq.java
+++ src/main/java/com/project/catxi/chat/dto/ChatMessageSendReq.java
@@ -1,11 +1,14 @@
 package com.project.catxi.chat.dto;
 
+import com.project.catxi.common.domain.MessageType;
+
 public record ChatMessageSendReq(
 	Long roomId,
 	String email,
-	String message
+	String message,
+	MessageType msgType
 ) {
 	public ChatMessageSendReq withRoomId(Long newRoomId) {
-		return new ChatMessageSendReq(newRoomId, this.email, this.message);
+		return new ChatMessageSendReq(newRoomId, this.email, this.message,this.msgType);
 	}
 }
