f76057e4594259ac2e4ab4c7aebe050e26d82c56
Dobby19 <southvi1@naver.com>
2025-07-02 10:42:41 +0900

    [FIX] Enum DB 매핑 분리

--- src/main/java/com/project/catxi/common/domain/MemberStatus.java
+++ src/main/java/com/project/catxi/common/domain/MemberStatus.java
@@ -1,5 +1,24 @@
 package com.project.catxi.common.domain;
 
 public enum MemberStatus {
-  PENDING,ACTIVE,INACTIVE
+  ACTIVE("ACTIVE"),PENDING("PENDING"),INACTIVE("INACTIVE");
+
+  private String code;
+  private MemberStatus(String code) {
+    this.code = code;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public static MemberStatus fromCode(String code) {
+    for (MemberStatus status : values()) {
+      if (status.getCode().equals(code)) {
+        return status;
+      }
+    }
+    throw new IllegalArgumentException("Unknown MEMBERSTATUS: " + code);
+  }
+
 }
