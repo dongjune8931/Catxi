17bc6b7df919e71404c7b703955ae2dc1c40e3f1
LGY010011 <gylm1111pm@gmail.com>
2025-08-05 14:52:12 +0900

    [CHORE] 파일 이름 변경

--- src/main/java/com/project/catxi/common/api/error/MapErrorCode.java
+++ src/main/java/com/project/catxi/common/api/error/MapErrorCode.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum MapError implements ErrorCode{
+public enum MapErrorCode implements ErrorCode{
 
 	COORDINATE_PARSE_FAILED(HttpStatus.BAD_REQUEST, "MAP400", "좌표 파싱에 실패했습니다."),
 	COORDINATE_NOT_FOUND(HttpStatus.NOT_FOUND, "MAP404", "좌표를 찾을 수 없습니다."),
 	COORDINATE_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MAP500", "좌표 저장에 실패했습니다."),
-	DEPARTURE_NOT_FOUND(HttpStatus.NOT_FOUND, "MAP404", "출발지 좌표를 찾을 수 없습니다.");
+	DEPARTURE_NOT_FOUND(HttpStatus.NOT_FOUND, "MAP404", "출발지 좌표를 찾을 수 없습니다. 출발지 좌표 저장 API를 호출해주세요.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
