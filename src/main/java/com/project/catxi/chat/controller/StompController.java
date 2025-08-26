d7242bc9a58c1eb80c1a24856674604b60e1c7fd
Dongjun <ldj990517@gmail.com>
2025-08-27 01:56:59 +0900

    [FIX] 중복 빈 제거

--- src/main/java/com/project/catxi/chat/controller/StompController.java
+++ src/main/java/com/project/catxi/chat/controller/StompController.java
@@ -2,6 +2,8 @@ package com.project.catxi.chat.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,30 +11,25 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.project.catxi.chat.dto.ChatMessageSendReq;
 import com.project.catxi.chat.service.ChatMessageService;
 import com.project.catxi.chat.service.RedisPubSubService;
 
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
 public class StompController {
 
 	private final SimpMessageSendingOperations messageTemplate;
 	private final ChatMessageService chatMessageService;
-	private final RedisPubSubService pubSubService;
+	private final @Qualifier("chatPubSub") StringRedisTemplate redisTemplate;
 	private final ObjectMapper objectMapper;
 
-	public StompController(SimpMessageSendingOperations messageTemplate, ChatMessageService chatMessageService,
-		RedisPubSubService pubSubService,ObjectMapper objectMapper) {
-		this.messageTemplate = messageTemplate;
-		this.chatMessageService = chatMessageService;
-		this.pubSubService = pubSubService;
-		this.objectMapper=objectMapper;
-	}
 
 
 
@@ -48,6 +45,6 @@ public class StompController {
 			LocalDateTime.now()
 		);
 		String message = objectMapper.writeValueAsString(enriched);
-		pubSubService.publish("chat", message);
+		redisTemplate.convertAndSend("chat", message);
 	}
 }
