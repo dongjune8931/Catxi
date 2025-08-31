f147ae115924542c4ecea55cb5a56dda2bfc7d1c
Dobby19 <southvi1@naver.com>
2025-09-01 00:37:04 +0900

    [FEAT] CookieUtil 추가

---
+++ src/main/java/com/project/catxi/common/auth/infra/CookieUtil.java
@@ -0,0 +1,32 @@
+package com.project.catxi.common.auth.infra;
+
+import java.time.Duration;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+  private static final String REFRESH_TOKEN_COOKIE_NAME = "refresh";
+
+  public static ResponseCookie createCookie(String token, Duration ttl) {
+
+    return ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, token)
+        .httpOnly(true)
+        .secure(true)
+        .path("/")
+        .maxAge(ttl)
+        .sameSite("None")
+        .build();
+  }
+
+  public static ResponseCookie deleteCookie() {
+    return ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, "")
+        .httpOnly(true)
+        .secure(true)
+        .path("/")
+        .maxAge(0) // 즉시 만료
+        .sameSite("None")
+        .build();
+  }
+}
