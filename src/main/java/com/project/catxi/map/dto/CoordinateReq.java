051860fa8212a27cb32351b699947dffd41b3aff
LGY010011 <gylm1111pm@gmail.com>
2025-09-05 17:05:51 +0900

    [FIX] 좌표 반환/응답 값에 필드 추가

--- src/main/java/com/project/catxi/map/dto/CoordinateReq.java
+++ src/main/java/com/project/catxi/map/dto/CoordinateReq.java
@@ -3,6 +3,8 @@ package com.project.catxi.map.dto;
 public record CoordinateReq(
 	Long roomId,
 	String email,
+	String name,
+	String nickname,
 	Double latitude,
 	Double longitude
 ) { }
--- src/main/java/com/project/catxi/map/dto/CoordinateRes.java
+++ src/main/java/com/project/catxi/map/dto/CoordinateRes.java
@@ -3,6 +3,8 @@ package com.project.catxi.map.dto;
 public record CoordinateRes (
 	Long roomId,
 	String email,
+	String name,
+	String nickname,
 	Double latitude,
 	Double longitude,
 	Double distance
