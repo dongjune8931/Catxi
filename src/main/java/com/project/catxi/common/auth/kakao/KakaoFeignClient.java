da9f87b4080e72b03bf673326d7364bc69e5b836
Dobby19 <southvi1@naver.com>
2025-09-06 00:02:34 +0900

    [FIX] kakaoFeign 수정

--- src/main/java/com/project/catxi/common/auth/kakao/KakaoFeignClient.java
+++ src/main/java/com/project/catxi/common/auth/kakao/KakaoFeignClient.java
@@ -4,12 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "kakaoFeignClient", url = "https://kapi.kakao.com")
 public interface KakaoFeignClient {
 
     @PostMapping(value = "/v1/user/unlink", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-        KakaoUnlinkRes unlinkUser(
-            @RequestHeader("Authorization") String authorization
+    KakaoUnlinkRes unlinkUser(
+        @RequestHeader("Authorization") String authorization,
+        @RequestParam("target_id_type") String targetIdType,
+        @RequestParam("target_id") Long targetId
     );
 }
