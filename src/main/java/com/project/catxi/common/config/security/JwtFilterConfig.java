409bcd5c23fb91dbe60b86318b33543b7ddbc596
Dobby19 <southvi1@naver.com>
2025-09-09 15:16:43 +0900

    [FIX] jwtFilterConfig 수정

--- src/main/java/com/project/catxi/common/config/security/JwtFilterConfig.java
+++ src/main/java/com/project/catxi/common/config/security/JwtFilterConfig.java
@@ -1,6 +1,7 @@
 package com.project.catxi.common.config.security;
 
 import com.project.catxi.common.auth.infra.TokenBlacklistRepository;
+import com.project.catxi.common.auth.service.TokenService;
 import com.project.catxi.common.jwt.JwtFilter;
 import com.project.catxi.common.jwt.JwtUtil;
 import com.project.catxi.member.repository.MemberRepository;
@@ -12,13 +13,13 @@ import org.springframework.stereotype.Component;
 public class JwtFilterConfig {
 
   private final JwtUtil jwtUtil;
-  private final JwtConfig jwtConfig;
+  private final TokenService tokenService;
   private final MemberRepository memberRepository;
   private final TokenBlacklistRepository tokenBlacklistRepository;
 
-  public JwtFilterConfig(JwtUtil jwtUtil, JwtConfig jwtConfig, MemberRepository memberRepository, TokenBlacklistRepository tokenBlacklistRepository) {
+  public JwtFilterConfig(JwtUtil jwtUtil, TokenService tokenService, MemberRepository memberRepository, TokenBlacklistRepository tokenBlacklistRepository) {
     this.jwtUtil = jwtUtil;
-    this.jwtConfig = jwtConfig;
+    this.tokenService = tokenService;
     this.memberRepository = memberRepository;
     this.tokenBlacklistRepository = tokenBlacklistRepository;
   }
@@ -26,7 +27,7 @@ public class JwtFilterConfig {
   public void configureJwtFilters(HttpSecurity http) throws Exception {
     // JwtFilter - 토큰 검증 및 인증 객체 설정
     http.addFilterBefore(
-        new JwtFilter(jwtUtil, memberRepository, tokenBlacklistRepository), UsernamePasswordAuthenticationFilter.class);
+        new JwtFilter(jwtUtil, tokenService, memberRepository, tokenBlacklistRepository), UsernamePasswordAuthenticationFilter.class);
   }
 
 }
